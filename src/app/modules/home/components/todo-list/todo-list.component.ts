11761c785a3f6ce0f505c43957da8dd95a49c417
Reginaldo da Silva Borges <reginaldo.borges@totvs.com.br>
2023-10-19 07:59:57 -0300

    Handson_7 com intrface de array e tasklist de objetos

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
+
+  public taskList: Array<TaskList> = [
+    {task: "Nova task 1", checked: true},
+    {task: "Nova task 2", checked: false},
+  ];
 
 }
