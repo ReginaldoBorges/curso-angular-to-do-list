5160cdf6c9e6c900e4cd74228af598c7da951aa0
Reginaldo da Silva Borges <reginaldo.borges@totvs.com.br>
2023-10-20 07:15:38 -0300

    Handson_8 implementada a exclusão por item e total da tasklist

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 // Interface
 import { TaskList } from '../../model/task-list';
+import { withDisabledInitialNavigation } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -14,5 +15,16 @@ export class TodoListComponent {
     {task: "Nova task 1", checked: true},
     {task: "Nova task 2", checked: false},
   ];
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Deseja deletar tudo?");
+    if(confirm) {
+      this.taskList = [];
+    }
+  }
 
 }
