11e733e249ed642640f5aa936caf92b7b17e8a17
reginaldo borges <reginaldo.borges07@gmail.com>
2023-10-21 17:05:13 -0300

    handson_10 implementações validationInput() e ngDoCheck()

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 // Interface
 import { TaskList } from '../../model/task-list';
 import { withDisabledInitialNavigation } from '@angular/router';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
 
+  ngDoCheck(): void {
+    this.taskList.sort( (first, last) => (Number(first.checked) - Number(last.checked)) );
+  }
+
   public setEmitTasklist(event: string) {
     this.taskList.push({task: event, checked: false});
+  }
+
+  public validationInput(event: string, index: number) {
+    if(!event.length) {
+      const confirm = window.confirm("Task está vazia, deseja deletar?");
+
+      if(confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 
   public deleteItemTaskList(event: number) {
